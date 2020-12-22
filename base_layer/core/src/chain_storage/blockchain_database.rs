c76dc20187ded0f2d889025249171a0798ba4c4b
Mike the Tike <stringhandler@gmail.com>
2020-12-22 12:00:55 +0200

    Update base_layer/core/src/chain_storage/blockchain_database.rs

--- base_layer/core/src/chain_storage/blockchain_database.rs
+++ base_layer/core/src/chain_storage/blockchain_database.rs
@@ -494,7 +494,7 @@ where B: BlockchainBackend
     }
 
 
-    /// Returns a connected header in the main chain by block hahs
+    /// Returns a connected header in the main chain by block hash
     pub fn fetch_chain_header_by_block_hash(&self, hash: HashOutput) -> Result<Option<ChainHeader>, ChainStorageError> {
         let db = self.db_read_access()?;
 
